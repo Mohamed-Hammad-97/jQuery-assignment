fe7139b0cf0e0bc24323a6809f1bb90f363e1615
Mohamed-Hammad-97 <mohamedhammad2997@gmail.com>
2023-08-21 12:37:48 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,69 @@
+$(".openNav").click(function () {
+    $("#leftMenu").animate({ width: '250px' }, 50)
+    $(".content").animate({ marginLeft: '250px' }, 500)
+    $(".open").animate({ marginLeft: '250px' }, 500)
+})
+
+$(".closebtn").click(function () {
+    $("#leftMenu").animate({ width: '0px' }, 50)
+    $(".content").animate({ marginLeft: '0px' }, 500)
+    $(".open").animate({ marginLeft: '0px' }, 500)
+});
+
+$("#leftMenu a").click(function () {
+
+    var sectionId = $(this).attr("href");
+
+    var positionOfSection = $(sectionId).offset().top;
+
+    $("html , body").animate({ scrollTop: positionOfSection }, 200);
+
+})
+
+
+
+window.onload = function () {
+
+    countDownToTime("1 october 2023 00:00:00");
+}
+
+function countDownToTime(countTo) {
+
+    let futureDate = new Date(countTo);
+    futureDate = (futureDate.getTime() / 1000);
+
+    let now = new Date();
+    now = (now.getTime() / 1000);
+
+    timeDifference = (futureDate - now);
+
+    let days = Math.floor(timeDifference / (24 * 60 * 60));
+    let hours = Math.floor((timeDifference - (days * (24 * 60 * 60))) / 3600);
+    let mins = Math.floor((timeDifference - (days * (24 * 60 * 60)) - (hours * 3600)) / 60);
+    let secs = Math.floor((timeDifference - (days * (24 * 60 * 60)) - (hours * 3600) - (mins * 60)))
+
+
+    $(".days").html(`${days} D`);
+    $(".hours").html(`${hours} h`);
+    $(".minutes").html(`${mins} m`);
+    $('.seconds').html(`${secs} s`)
+
+
+    setInterval(function () { countDownToTime(countTo); }, 1000);
+}
+
+
+
+var maxLength = 100;
+$('textarea').keyup(function () {
+    var length = $(this).val().length;
+    var AmountLeft = maxLength - length;
+    if (AmountLeft <= 0) {
+        $("#chars").text("your available character finished");
+
+    }
+    else {
+
+        $("#chars").text(AmountLeft);
+    }
+});
